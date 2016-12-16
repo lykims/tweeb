90f52252b732b305ee3d94d5311b2e60cf8229e8
Kim S. Ly <lykims@users.noreply.github.com>
2016-12-15 23:24:44 -0500

    Change implementation to accept txt file as argument and not hashtags.

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
 # Twitter Bot built with Node.js
 
-The Twitter bot likes tweets based on hashtags passed as arguments.
+The Twitter bot likes tweets based on hashtags.
 
 
 ## Getting Started
 
-Clone the repository and install packages :
+* Clone the repository and install packages :
 
 ```
 $ npm install
 ```
 
-Set keys and tokens in config.js.
+* Set keys and tokens in config.js.
 
-Run the program with the following command :
+* Create a text file in the same directory and type hashtags in the file.
+
+* Run the program with the following command :
 
 ```
-$ npm start <interval in minutes> <hashtags separated by space>
+$ node bot.js <interval in minutes> <text file containing hashtags>
+```
+
+* The program can also be run with `npm scripts`. In `package.json`, set the default command in `start` and then run the following command :
+
+```
+$ npm start
 ```
--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var dateFormat = require('dateformat');
+var read = require('read-file');
 var twit = require('twit');
 var config = require('./config.js');
 
@@ -7,33 +8,72 @@ var Twitter = new twit(config);
 var intervalTime = 10; // By default, 10 minutes
 var hashtags = '';
 
+var exitOnError = function(errorMessage) {
+    console.log();
+    if(errorMessage) {
+        console.log(errorMessage);
+    }
+    console.log('Usage : node bot.js <interval in minutes> <text file containing hashtags>\n');
+    process.exit(-1);
+}
+
 var isNumber = function(value) {
     return /^\d+$/.test(value);
 }
 
-var setArguments = function(args) {
-    if (args.length <= 3) {
-        console.log("Usage: node bot.js <interval in minutes> <hashtags separated by space>");
-        process.exit(-1);
+var isTextFile = function(filename) {
+    var extension = filename.trim().split('.').pop();
+    return extension && extension.toLowerCase() === 'txt'
+}
+
+var setIntervalTime = function(arg) {
+    if (isNumber(arg)) {
+        intervalTime = arg * 60000;
     }
     else {
-        if (isNumber(args[2])) {
-            intervalTime = args[2] * 60000;
-        }
-        else {
-            console.log("The second argument should be a positive integer.");
-            console.log("Usage: node bot.js <interval in minutes> <hashtags separated by space>");
-            process.exit(-1);
-        }
-
-        for (i = 3; i < args.length; i++) {
-            hashtags += '#' + args[i];
-            if(i < (args.length-1) ) {
-                hashtags += ', '
-            }
-        }
+        exitOnError("The second argument should be a positive integer.");
     }
 }
+
+var setHashtags = function(filename) {
+    if(isTextFile(filename)) {
+        var buffer = read.sync(filename, {encoding: 'utf8'});
+        if(buffer) {
+            var words = buffer.trim().split(/[#\s]+/g);
+            if(words.length === 1 && words[0] === '') {
+                exitOnError('The file is empty. It should contain hashtags.');
+            }
+            else {
+                var hashwords = words.map(function(el) { return '#' + el; });
+                hashtags = hashwords.join(', ');
+                console.log('\n----- HASHTAGS -----');
+                console.log(hashtags);
+                console.log('--------------------\n');
+            }
+        }
+        else {
+            exitOnError('The file is empty. It should contain hashtags.');
+        }
+    }
+    else {
+        exitOnError('The file extension should be ".txt"');
+    }
+}
+
+var processArguments = function(args) {
+    if (args.length <= 3) {
+        exitOnError('');
+    }
+    else {
+        setIntervalTime(args[2]);
+        setHashtags(args[3]);
+    }
+}
+
+var rand = function(arr) {
+  var index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+};
 
 var like = function() {
     var params = {
@@ -48,24 +88,27 @@ var like = function() {
             console.log(err);
         }
         else {
-            var tweetId = data.statuses[0].id_str;
-            Twitter.post('favorites/create', {
-                id: tweetId
-            }, function(err, response) {
-                var now = new Date();
-                if (response) {
-                    console.log(dateFormat(now, 'yyyy-mm-dd h:MM:ss TT') + ' : Successfully liked the tweet ID ' + tweetId + '! :D');
-                }
-                if (err) {
-                    console.log(dateFormat(now, 'yyyy-mm-dd h:MM:ss TT') + ' : Like failed. :(');
-                    console.log(err);
-                }
-            });
+            var randomTweet = rand(data.statuses);
+            if(typeof randomTweet != 'undefined') {
+                var tweetId = randomTweet.id_str;
+                Twitter.post('favorites/create', {
+                    id: tweetId
+                }, function(err, response) {
+                    var now = new Date();
+                    if (response) {
+                        console.log('> ' + dateFormat(now, 'yyyy-mm-dd h:MM:ss TT') + ' : Successfully liked the tweet ID ' + tweetId + '! :D\n');
+                    }
+                    if (err) {
+                        console.log('> ' + dateFormat(now, 'yyyy-mm-dd h:MM:ss TT') + ' : Like failed. :(');
+                        console.log(err + '\n');
+                    }
+                });
+            }
         }
     });
 }
 
-setArguments(process.argv);
+processArguments(process.argv);
 
 like();
 
